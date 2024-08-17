9b3810c6a8072c379c951f84cd3b5b4020e75b4f
farangiz77 <fireer4878@gmail.com>
2024-08-17 16:52:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+
+// const color = prompt("choose color: red, blue or green")
+
+
+// const box = document.querySelector(".box")
+// console.log(box);
+
+
+// if (color === "red"){
+//     box.classList.add("red")
+// }else if (color ==="green") {
+//     box.classList.add("green")
+// } else if (color ==="blue") {
+//     box.classList.add("blue")
+// } else {
+//     alert("vi vveli nekorrektniy cvet")
+// }
+
+// const title = document.querySelector("h1")
+
+// const newText = prompt("vvedite noviy text")
+
+// if(newText) {
+//     title.textContent = newText
+// } else {
+//     alert("vi nichego ne veli")
+// }
+
+// const name = document.querySelector(".box h1")
+// const surname = document.querySelector(".box h2")
+// const age = document.querySelector(".box h3")
+// const image = document.querySelector(".box img")
+
+// const firstName = prompt("ur name")
+// const lastName = prompt("ur surname")
+// const enterAge = prompt("ur age")
+// const enterImage = prompt("ur image")
+
+// name.textContent = firstName
+// surname.textContent = lastName
+// age.textContent = enterAge
+// image.src = enterImage
+
